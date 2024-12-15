2fa87a051f96779db5b5b93132a84d20fb24e8c0
fromo0214 <faromo0214@gmail.com>
2024-12-14 20:53:14 -0800

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Fernando Romo on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
