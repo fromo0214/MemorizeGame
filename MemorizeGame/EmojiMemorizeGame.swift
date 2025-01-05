dceb5185bbe64e3bfbda16348618bc389da438ff
fromo0214 <faromo0214@gmail.com>
2025-01-04 18:01:39 -0800

    Created ViewModel, added different themes for cards and shuffled upon playing.

---
+++ MemorizeGame/EmojiMemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemorizeGame.swift
+//  MemorizeGame
+//
+//  Created by Fernando Romo on 1/2/25.
+//
+
+import Foundation
---
+++ MemorizeGame/MemorizeGameLogic.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGameLogic.swift
+//  MemorizeGame
+//
+//  Created by Fernando Romo on 1/2/25.
+//
+
+import Foundation
