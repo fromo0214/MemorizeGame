151d754ee9c1d3ba48311218e1b78cde7a9f83c5
fromo0214 <faromo0214@gmail.com>
2025-02-25 16:05:50 -0800

    AspectVGrid added. @ViewBuilder

---
+++ AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  MemorizeGame
+//
+//  Created by Fernando Romo on 2/25/25.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
